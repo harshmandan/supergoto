e1cae3a4c4f3b364543248e809725302bafe4f14
Harsh Mandan <harshsinghmandan@gmail.com>
2023-03-17 09:03:06 +0530

    Add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import Footer from '$components/footer/Footer.svelte';
 	import Navbar from '$components/navbar/Navbar.svelte';
 	import '../app.css';
 </script>
 
 <Navbar />
-<slot />
+<div class="my-8">
+	<slot />
+</div>
+<Footer />
